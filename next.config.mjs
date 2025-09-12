95fad97b414a0befd21b2b5041dad96c9204b3b1
Yuvraj Manchadi <yuvrajmanchadi321@gmail.com>
2025-09-12 12:03:30 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xncvaqkigtbkidapzvzm.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabinImages/**",
+      },
+    ],
+  },
+  // output: "export",
+};
+
+export default nextConfig;
